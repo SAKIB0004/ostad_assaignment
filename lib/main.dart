86cec9cee57705a10f0bb19ab4f80c92f5bc0fc5
Ostad_B_8 <mahmudulhaque540@gmail.com>
2024-11-10 12:22:33 +0600

    Assignment 2 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,86 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-void main(){
+void main() {
   runApp(myApp());
 }
 
-
-class myApp extends StatelessWidget{
+class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Hello World",
-      home: home(),
+      home: myProfile(),
     );
   }
-
 }
 
-class home extends StatelessWidget{
+class myProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text("My Profile"),
         backgroundColor: Colors.amber,
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+          //SizedBox(width: 20,),
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+          //SizedBox(width: 20,),
+          IconButton(onPressed: () {}, icon: Icon(Icons.call)),
+          //SizedBox(width: 10,),
+        ],
       ),
-
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 90,
+            backgroundColor: Colors.purple.shade100,
+            child: Icon(
+              Icons.icecream,
+              size: 80,
+            ),
+          ),
+          Text(
+            "Ice cream is very delicious right?",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          SizedBox(
+            height: 35,
+          ),
+          CircleAvatar(
+            radius: 90,
+            backgroundColor: Colors.purple.shade100,
+            child: Icon(
+              Icons.code,
+              size: 80,
+            ),
+          ),
+          Text(
+            "Programming is not boring if you love it",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          SizedBox(
+            height: 35,
+          ),
+          CircleAvatar(
+            radius: 90,
+            backgroundColor: Colors.purple.shade100,
+            child: Icon(
+              Icons.egg,
+              size: 80,
+            ),
+          ),
+          Text(
+            "If you submit code directly copy from chatgpt then mark will 0",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          )
+        ],
+      ),
     );
   }
-
 }
