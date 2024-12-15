53d965e731b819bd3edfb1f56caf000f27936490
Ostad_B_8 <mahmudulhaque540@gmail.com>
2024-12-15 22:01:14 +0600

    Assignment 3 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'app.dart';
 
-void main(){
-  runApp(myApp());
-}
-
-class myApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Hello World",
-      home: home(),
-    );
-  }
-
-}
-
-
-
-class home extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        backgroundColor: Colors.amber,
-      ),
-
-    );
-  }
-
+void main() {
+  runApp(const CRUDApp());
 }
